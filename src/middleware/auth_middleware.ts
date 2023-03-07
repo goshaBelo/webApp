ff6f81822a2b570658a80da350b0ea1d60a7ff0b
Gosha Student <yaren042@mail.ru>
2023-03-07 09:00:53 +0500

    add authentication middleware function

--- src/middleware/auth_middleware.ts
+++ src/middleware/auth_middleware.ts
@@ -1,10 +1,25 @@
-import * as jwt from "jwonwebtoken"
+import * as jwt from "jsonwebtoken"
+import {postgresClient} from "../postgre/postgresClient"
 
 async function authMiddleware(request, response, next){
 	try{
-		console.log(request.headers)
-
+		//console.log(request.headers)
+		let token = request.headers.authentication
+		if(token){
+			let dataFromToken = jwt.verify(token, "SECRET_KEY")
+			let user = (await postgresClient.query(`SELECT * FROM users WHERE id='${dataFromToken.id}2';`)).rows[0]
+			if(user){
+				next()
+			}else{
+				response.send("authentication error2")
+			}
+		}else{
+			response.send("authentication error")
+		}
 	}catch(error){
 		console.log(error)
+		response.send("authentication error")
 	}
 }
+
+export {authMiddleware}
