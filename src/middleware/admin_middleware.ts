d6f37143f06a82fdc45ada5deb5bf5ffdad5df00
Gosha Student <yaren042@mail.ru>
2023-03-07 10:38:14 +0500

    add check admin middleware

---
+++ src/middleware/admin_middleware.ts
@@ -0,0 +1,22 @@
+import * as express from "express"
+import {postgresClient} from "../postgre/postgresClient"
+
+async function adminMiddleware(request, response, next){
+	try{
+		let user = request.user
+		console.log("from adminMiddleware",user)
+		let group_id = request.params.id
+		let admin = (await postgresClient.query(
+			`SELECT * FROM group_admins WHERE group_id='${group_id}' AND user_id='${user.id}';`)).rows[0]
+		if(admin){
+			next()
+		}else{
+			response.send("Error/you are not admin of this group/ Error")
+		}
+
+	}catch(error){
+		console.log(error)
+	}
+}
+
+export {adminMiddleware}
